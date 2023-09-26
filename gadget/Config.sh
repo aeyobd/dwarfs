db6d6076bc227613f614c1033eed4fde94abcb1e
Daniel Boyea <aeyobd@gmail.com>
2023-09-26 12:47:23 -0700

    added config

---
+++ gadget/Config.sh
@@ -0,0 +1,28 @@
+
+# Basic code operation
+
+    LEAN
+    SELFGRAVITY
+    
+# Gravity options
+
+    # PMGRID=512
+    # TREEPM_NOTIMESPLIT
+    # ASMTH=2.0
+    
+# Softening types and particle types
+    NSOFTCLASSES=1
+    NTYPES=2
+
+# Floating point accuracy
+
+    POSITIONS_IN_64BIT
+    DOUBLEPRECISION=1
+
+# Group finding
+
+    FOF
+
+# Miscellaneous code options
+
+
