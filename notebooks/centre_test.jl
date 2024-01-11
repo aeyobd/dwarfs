e16a439a588f4db4afcf29976ece6d3e111c5607
aeyobd <aeyobd@gmail.com>
2024-01-11 00:54:24 +0000

    (once) working ss centre and now renaming for clearer names

--- notebooks/centre_test.jl
+++ notebooks/centre_test.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.32
+# v0.19.36
 
 using Markdown
 using InteractiveUtils
@@ -11,39 +11,70 @@ begin
 	using Plots
 	plotlyjs()
 
-	using LilGuys
-	using Optim
-	using StatsBase: mean, std
+	import LilGuys as lguys
+	import StatsBase: mean, std
 end
 
 # ╔═╡ ddd5502b-5c4a-4608-93c8-511101dd5dfe
-out = Output("/cosma/home/durham/dc-boye1/data/models/sculptor/isolation/out/")
+out = lguys.Output("/cosma/home/durham/dc-boye1/data/models/fornax/mw/out")
 
 # ╔═╡ 69b291b2-2589-489e-9a9b-19bdbc12114f
 begin
-snap_i = out[1, :]
-snap_f = out[end, :]
+	snap_i = out[1, :]
+	snap_m = out[100, :]
+	snap_f = out[end, :]
 end
 
-# ╔═╡ efaf3eda-6de3-4b36-bbea-e4002b6c3d6f
-maximum(snap_i.Φ)
+# ╔═╡ bb395b6a-7507-4f4e-a526-661881a2dff1
+begin
+	cen_i = lguys.ss_centre(snap_i)
+	cen_f = lguys.ss_centre(snap_f)
+end
+
+# ╔═╡ d0923b9a-a822-4324-aca6-4044655986ea
+cen_f.N
+
+# ╔═╡ 620c8b0a-e399-4976-ab09-313beeb7596f
+cen_f.xs
+
+# ╔═╡ 52716936-fea3-4cee-b6db-2079228a38eb
+cen_i.x_c[1:1]
 
 # ╔═╡ 41bec3db-f68e-480c-8eb2-8245a6d0f8ef
-scatter(snap_i.pos[1, :], snap_i.pos[2, :])
+begin
+	scatter(snap_i.pos[1, :], snap_i.pos[2, :], ma=0.05, lw=0, msa=0, ms=5)
+	scatter!(cen_i.x_c[1:1], cen_i.x_c[2:2])
+end
 
 # ╔═╡ d86be9ed-2010-4d56-a031-57acaba94921
-scatter(snap_f.pos[1, :], snap_f.pos[2, :])
+begin
+	scatter(snap_f.pos[1, :], snap_f.pos[2, :], ma=0.05, lw=0, msa=0, ms=5)
+	scatter!(cen_f.x_c[1:1], cen_f.x_c[2:2])
+
+end
+
+# ╔═╡ 304e436b-6ceb-4212-bd0e-7cef47b6da5e
+begin
+	cens = []
+	for i in 1:size(out, 1)
+		println(i)
+		
+		snap_a = out[i, :]
+		cen = lguys.ss_centre(snap_a)
+		push!(cens, cen.x_c)
+	end
+end
 
 # ╔═╡ 943d7709-4082-4f99-b17e-701fcb82edab
-snap_i.index
+cen_f.x_c[1:1], cen_f.x_c[2:2]
 
 # ╔═╡ 13c83390-a33a-4d92-9cc8-57f552b70446
 begin 
-	p0 = mean(snap_f.pos, dims=2)
-	v0 = mean(snap_f.vel, dims=2)
+	p0 = lguys.centroid(snap_f.pos)
+	v0 = lguys.centroid(snap_f.vel)
 	
-	rs = LilGuys.r(snap_f.pos .- p0)
-	vs = LilGuys.r(snap_f.vel .- v0)
+	rs = lguys.calc_r(snap_f.pos .- p0)
+	vs = lguys.calc_r(snap_f.vel .- v0)
 	# vs_a = LilGuys.get_r(Matrix(transpose(reshape(snap_i.vel, (10_000, 3)))))
 	# rs_a = LilGuys.get_r(Matrix(transpose(reshape(snap_i.pos, (10_000, 3)))))
 
@@ -54,22 +85,22 @@ end
 idx = argmax(rs)
 
 # ╔═╡ 7d09ea26-c5fb-432f-8b21-b4983db0b13a
-vs1 = LilGuys.r(snap_f.vel .- snap_f.vel[idx])
+vs1 = lguys.calc_r(snap_f.vel .- snap_f.vel[idx])
 
 # ╔═╡ e80ffd4b-8a4d-4e33-9f0e-e0f1f41a99ae
 histogram(vs1)
 
 # ╔═╡ 781dd39a-aca8-4f46-bc15-95a10d766edf
-rs1 = LilGuys.r(snap_f.pos .- snap_f.pos[idx])
+rs1 = lguys.calc_r(snap_f.pos .- snap_f.pos[idx])
 
 # ╔═╡ 9d688a4c-f825-47f9-9a0f-f1a233c09835
 histogram(log10.(rs1))
 
 # ╔═╡ 6aaa45fb-1610-421e-932f-e0a97613581f
-std(LilGuys.r(snap_f.pos .- p0)) / length(rs)^0.5
+std(lguys.calc_r(snap_f.pos .- p0)) / length(rs)^0.5
 
 # ╔═╡ ba1182dd-7b42-4483-9044-66c7c4fe36e7
-std(LilGuys.r(snap_f.vel .- v0)) / length(vs)^0.5
+std(lguys.calc_r(snap_f.vel .- v0)) / length(vs)^0.5
 
 # ╔═╡ e8846565-0ae8-4e0d-a77e-8176225713e5
 snap_f.pos[:, idx]
@@ -82,7 +113,7 @@ snap_f.acc
 
 # ╔═╡ a8c405cc-a504-46ca-b137-aa203bd9f7fd
 begin
-	v = LilGuys.r(snap_f.vel[idx:idx] .- v0)[1]
+	v = lguys.calc_r(snap_f.vel[idx:idx] .- v0)[1]
 	pot = snap_f.Φ[idx]
 	e = pot + 0.5v^2
 	e_l = pot + 0.5*(v-2*0.075)^2
@@ -96,13 +127,13 @@ histogram(snap_f.Φ)
 scatter(rs, snap_f.Φ)
 
 # ╔═╡ 14a4c719-4820-45e7-8c5b-0d15eee3cdd7
-ϵs = LilGuys.E_spec(snap_f)
+ϵs = lguys.calc_E_spec(snap_f)
 
 # ╔═╡ f054630a-0cd2-429e-991b-455c0e06a0df
 histogram(ϵs)
 
 # ╔═╡ 16a6cabe-5008-4ce1-96bf-f1271e422a1d
-dxs, dvs = LilGuys.knn_err(snap_f)
+dxs, dvs = lguys.phase_volumes(snap_f)
 
 # ╔═╡ f4588f02-8abd-4ebc-8eaf-1924fdc47669
 function circle(x, y, r=1; n=30)
@@ -115,7 +146,7 @@ begin
 	circles = Plots.Shape[]
 	snap = snap_f
 	k = 100
-	rs2 = LilGuys.r(snap.pos)
+	rs2 = lguys.calc_r(snap.pos)
 	r_cut = sort(rs2)[k]
 	filt = rs .< r_cut
 	snap_filt = snap_f[filt]
@@ -141,7 +172,7 @@ begin
 		x = snap_filt.vel[1, i]
 		y = snap_filt.vel[2, i]
 		z = snap_filt.vel[3, i]
-		r = LilGuys.r(snap_filt.pos[:, i])
+		r = lguys.calc_r(snap_filt.pos[:, i])
 
 		r = dvs[i]
 		push!(circles2, circle(x, y, r))
@@ -152,16 +183,16 @@ begin
 end
 
 # ╔═╡ 445924a7-fd07-43b4-9939-5f2bb34fc51a
-snap2 = Snapshot("/cosma/home/durham/dc-boye1/data/models/fornax/mw/out/snapshot_090.hdf5")
+snap2 = lguys.Snapshot("/cosma/home/durham/dc-boye1/data/models/fornax/mw/out/snapshot_090.hdf5")
 
 # ╔═╡ 1fff02a4-8746-4f8f-a1c4-4fb00e450dd5
-ps = LilGuys.bound_probabilities(snap2)
+ps = lguys.bound_probabilities(snap2)
 
 # ╔═╡ bb0ecbcd-2530-45c3-93e4-7b7925cce647
 histogram(ps)
 
 # ╔═╡ 6aa3a0e6-751b-4a87-97c9-2165a2ea4306
-es = LilGuys.E_spec(snap2)
+es = lguys.calc_E_spec(snap2)
 
 # ╔═╡ f42c5bee-1858-4e20-8e6e-ce4a7bb105e7
 scatter(asinh.(es), ps)
@@ -173,22 +204,37 @@ length(snap2)
 sum((0.1 .< ps) .&& (0.9 .> ps))
 
 # ╔═╡ 34250926-90dc-459f-9148-ea4043a172d1
-scatter(snap2.pos[2, :], snap2.pos[1, :], lw=0, ms=1, ma=0.05)
+begin
+	cen, ws = lguys.fuzzy_centre(snap2, LilGuys.centroid(snap2))
+	scatter(snap2.pos[2, :], snap2.pos[1, :], marker_z=ws, lw=0, ms=1, ma=0.05)
+
+	scatter!([cen.pos[2]], [cen.pos.x])
+end
+
+
+# ╔═╡ 2b27e20e-fb16-483a-b2f9-e87a4a57d723
+cen.pos
 
 # ╔═╡ 6ee1d6e7-4879-46b9-b05a-b50c2cc3f868
 begin 
-	xc, vc = LilGuys.energetic_center(snap2)
-	snap2.pos .-= xc
-	snap2.vel .-= vc
+	p_i = LilGuys.FuzzyPhase(zeros(3), zeros(3), 0, 0)
+	xc, vc = LilGuys.fuzzy_centre(snap2, p_i)
 end
+
+# ╔═╡ b1c34b1b-b0f3-4a69-b278-cc4cd894b52f
+
 
 # ╔═╡ Cell order:
 # ╠═06484816-d7f4-4389-b0bf-e5a95214082e
 # ╠═ddd5502b-5c4a-4608-93c8-511101dd5dfe
 # ╠═69b291b2-2589-489e-9a9b-19bdbc12114f
-# ╠═efaf3eda-6de3-4b36-bbea-e4002b6c3d6f
+# ╠═d0923b9a-a822-4324-aca6-4044655986ea
+# ╠═bb395b6a-7507-4f4e-a526-661881a2dff1
+# ╠═620c8b0a-e399-4976-ab09-313beeb7596f
+# ╠═52716936-fea3-4cee-b6db-2079228a38eb
 # ╠═41bec3db-f68e-480c-8eb2-8245a6d0f8ef
 # ╠═d86be9ed-2010-4d56-a031-57acaba94921
+# ╠═304e436b-6ceb-4212-bd0e-7cef47b6da5e
 # ╠═943d7709-4082-4f99-b17e-701fcb82edab
 # ╠═13c83390-a33a-4d92-9cc8-57f552b70446
 # ╠═8bebe29d-0157-490e-bf4c-bc847dc1c442
@@ -217,5 +263,7 @@ end
 # ╠═f42c5bee-1858-4e20-8e6e-ce4a7bb105e7
 # ╠═2720212c-55e0-49f6-825a-49f9e6ea0eef
 # ╠═0f006653-9f09-49b7-b19b-d35d804c75ab
+# ╠═2b27e20e-fb16-483a-b2f9-e87a4a57d723
 # ╠═34250926-90dc-459f-9148-ea4043a172d1
 # ╠═6ee1d6e7-4879-46b9-b05a-b50c2cc3f868
+# ╠═b1c34b1b-b0f3-4a69-b278-cc4cd894b52f
