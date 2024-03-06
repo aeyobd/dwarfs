dec363a19b145072b64c5b2bbae02f676b1cac9a
aeyobd <aeyobd@gmail.com>
2024-03-06 20:01:36 +0000

    orbits from apocentre

--- models/sculptor/mc_orbits/analyze.jl
+++ models/sculptor/mc_orbits/analyze.jl
@@ -9,12 +9,15 @@ begin
 	import Pkg
 	Pkg.activate()
 	using Plots
-	plotly()
 	using DataFrames, CSV
 
+	using Printf
 	import LilGuys as lguys
 end
 
+
+# ╔═╡ d975d00c-fd69-4dd0-90d4-c4cbe73d9754
+using Statistics
 
 # ╔═╡ a7ce5b0c-84a6-4d63-94f1-68e7a0d9e758
 if !@isdefined obs # read in sample (but only once)
@@ -61,17 +64,13 @@ begin
 	plots[]
 end
 
-# ╔═╡ a449025f-647c-4b68-afad-cefc2941a4da
-findfirst(isequal(lguys.percentile(peris, 16)), peris)
-
-# ╔═╡ d75d4521-f4d3-4162-a01f-830d6a4b85b8
-findfirst(isequal(lguys.percentile(peris, 84)), peris)
-
-# ╔═╡ 795cfff6-7951-4f2b-a956-a8f8c8c40113
-idx = [1, 2]
-
-# ╔═╡ 0b9cb669-ef83-44a3-b4a8-f1c2e07cba0b
-out.times
+# ╔═╡ e10615ab-0ece-4ebb-81d0-47ffdd6ea6c5
+begin
+	percies = [16, 84]
+	quantiles = lguys.percentile(peris, [16, 84])
+	idx_qs = [findfirst(isequal(p), peris) for p in quantiles]
+	idx = vcat([1], idx_qs)
+end
 
 # ╔═╡ 92b28dd8-c353-4959-b181-a843367b3223
 histogram(lguys.calc_r(snap.velocities))
@@ -140,6 +139,19 @@ end
 plots_scat
 end
 
+# ╔═╡ 4ee33ce2-c00a-4fcf-b7fc-b78c1677c9e4
+begin 
+	peri1 = lguys.percentile(peris, 10)
+	peri2 = lguys.percentile(peris, 20)
+
+	idx_s = @. peri1 < peris < peri2
+
+	for sym in [:distance, :pm_ra, :pm_dec, :radial_velocity, :ra, :dec]
+		md = median(getproperty.(observations[idx_s], sym))
+		println("d $sym = $(md - getproperty(obs, sym))")
+	end
+end
+
 # ╔═╡ e5825c4a-b446-44a3-8fd5-d94664965aca
 for f in fieldnames(lguys.Observation)
 	a = getproperty(observations[1], f)
@@ -158,9 +170,9 @@ begin
 	positions = lguys.extract(out, :positions, idx)
 	velocities = lguys.extract(out, :velocities, idx)
 	accelerations = lguys.extract(out, :accelerations, idx)
-	positions = [positions[:, i, :] for i in idx]
-	velocities = [velocities[:, i, :] for i in idx]
-	accelerations = [accelerations[:, i, :] for i in idx]
+	positions = [positions[:, i, :] for i in 1:length(idx)]
+	velocities = [velocities[:, i, :] for i in 1:length(idx)]
+	accelerations = [accelerations[:, i, :] for i in 1:length(idx)]
 	Φs_ext = lguys.extract(out, :Φs_ext, idx)
 	Φs = lguys.extract(out, :Φs, idx)
 
@@ -180,6 +192,9 @@ begin
 	hline!([peris[idx], apos[idx]])
 end
 
+# ╔═╡ ee01b25e-c32e-4f6e-96d6-cb9c6f3ea95c
+positions
+
 # ╔═╡ 130fca42-cee8-4d88-a764-cdded04a636e
 lguys.plot_xyz(positions...)
 
@@ -198,7 +213,7 @@ scatter(rs, accs, marker_z=out.times)
 # ╔═╡ 5d5f719c-e5b7-4e05-94b0-71523da46b66
 begin 
 	plots[] = [plot()]
-	for i in idx
+	for i in 1:length(idx)
 		scatter!(positions[i][1, :], accs[i])
 	end
 	plots[]
@@ -216,7 +231,7 @@ end
 # ╔═╡ fa4e7992-1ac6-4d71-a923-8b3cf81d0030
 begin 
 	plots[] = [plot()]
-	for i in idx
+	for i in 1:length(idx)
 		scatter!(out.times, accs[i] .- a_exp.(rs[i]))
 	end
 	plots[]
@@ -226,7 +241,7 @@ end
 begin 
 	plots[] = [plot()]
 	
-	for i in idx
+	for i in 1:length(idx)
 		scatter!(out.times, Φs_ext[i, :] .- phi_exp.(rs[i]))
 	end
 	plots[]
@@ -241,11 +256,86 @@ lguys.scatter_xyz(snap.positions)
 # ╔═╡ 5fdd8307-d528-4cd7-a5e4-1f15aba75cd5
 lguys.scatter_xyz(-snap.velocities .* lguys.V0)
 
-# ╔═╡ 9c82b9ce-57a5-4b16-8bd9-6458931ef6f5
-snap.positions
+# ╔═╡ 2dfe9a85-6553-4632-81e0-33c148fd1102
+reverse(out.times)
 
-# ╔═╡ 89357b44-82e3-4f84-9cf1-3c0c01d1cf03
-snap.velocities * lguys.V0
+# ╔═╡ 519a88f0-8e2d-4c09-83e0-3cc2ee147e35
+function get_initial_t(j)
+	i = idx[j]
+	N = length(rs[j])
+	t_ini = -1
+	for t in N-2:-1:1
+		if diff(rs[j])[t] >= 0 && diff(rs[j])[t+1] <= 0
+			t_ini = t
+			break
+		end
+	end
+	return t_ini
+end
+	
+
+# ╔═╡ 5f45e7c7-e447-48bf-ade4-38f516df2dad
+for i in 1:length(idx)
+	fname = "orbit$i.csv"
+	vel = -reverse(velocities[i], dims=2)
+	pos = reverse(positions[i], dims=2)
+	t0 = size(pos, 2) - get_initial_t(i)
+	println(t0)
+	t = out.times[end] .- reverse(out.times)
+
+	df = DataFrame(
+		t = t[t0:end],
+		x = pos[1, t0:end],
+		y = pos[2, t0:end],
+		z = pos[3, t0:end],
+		v_x = vel[1, t0:end],
+		v_y = vel[2, t0:end],
+		v_z = vel[3, t0:end],
+	)
+
+	println("saving to $fname")
+	CSV.write(fname, df)
+end
+
+# ╔═╡ 5316884b-3971-4ca7-9106-f638241d3388
+get_initial_t(1)
+
+# ╔═╡ 34b812d2-21c0-4983-9d14-7dbef08ab670
+begin 
+	plot()
+	plot!(rs[1])
+	hline!(apos[1:1])
+end
+
+# ╔═╡ de1e5245-0946-47cd-8e2c-ba1914cfeb74
+begin 
+	# orbit info
+	for i in 1:length(idx)
+		t = get_initial_t(i)
+		@printf "orbit: \t\t %i\n" i
+		@printf "index: \t\t %i\n" idx[i]
+		
+		@printf "pericentre:\t %0.1f\n" peris[idx[i]]
+		@printf "apocentre: \t %0.1f\n" apos[idx[i]]
+
+		@printf "time of first apocentre: %f \n" out.times[end] - out.times[t]
+		@printf "radius of first apocentre: %f\n" rs[i][t]
+		@printf "intial position: [%f, %f, %f]\n" positions[i][:, t]...
+		@printf "intial velocity: [%f, %f, %f]\n" -velocities[i][:, t]...
+		@printf "final position: [%f, %f, %f]\n" positions[i][:, 1]...
+		@printf "final velocity: [%f, %f, %f]\n" -velocities[i][:, 1]...
+
+		o = observations[idx[i]]
+		@printf "ra: \t %f\n" o.ra
+		@printf "dec: \t %f\n" o.dec
+		@printf "pmra: \t %f\n" o.pm_ra
+		@printf "pmdec: \t %f\n" o.pm_dec
+		@printf "dist: \t %f\n" o.distance
+		@printf "rv: \t %f\n" o.radial_velocity
+
+		println()
+	end
+end
 
 # ╔═╡ Cell order:
 # ╠═e9e2c787-4e0e-4169-a4a3-401fea21baba
@@ -256,10 +346,7 @@ snap.velocities * lguys.V0
 # ╠═4481a5e1-d635-4fea-a5c5-c85f0d6df62f
 # ╠═4d4a18fc-8990-4dcc-97c5-c9e01708ea2e
 # ╠═fcadcc96-1da1-4e6f-9d3b-2c56e55488b7
-# ╠═a449025f-647c-4b68-afad-cefc2941a4da
-# ╠═d75d4521-f4d3-4162-a01f-830d6a4b85b8
-# ╠═795cfff6-7951-4f2b-a956-a8f8c8c40113
-# ╠═0b9cb669-ef83-44a3-b4a8-f1c2e07cba0b
+# ╠═e10615ab-0ece-4ebb-81d0-47ffdd6ea6c5
 # ╠═92aac8e8-d599-4a1e-965e-e653bc54c509
 # ╠═e5d40e2f-ac47-4827-853d-2f94bc39a624
 # ╠═92b28dd8-c353-4959-b181-a843367b3223
@@ -271,10 +358,13 @@ snap.velocities * lguys.V0
 # ╠═ede3836c-740d-4ac7-bbc7-3165981a1878
 # ╠═ac81acd8-4a78-4230-bc70-3b78a861b618
 # ╠═d3063e30-2cb3-4f1b-8546-0d5e81d90d9f
+# ╠═d975d00c-fd69-4dd0-90d4-c4cbe73d9754
+# ╠═4ee33ce2-c00a-4fcf-b7fc-b78c1677c9e4
 # ╠═e5825c4a-b446-44a3-8fd5-d94664965aca
 # ╟─a644a33e-57a4-4af4-98be-1e0e84948069
 # ╠═d31f91e8-6db6-4771-9544-8e54a816ecc1
 # ╠═5be3fdaa-5c87-4fef-b0eb-06dfa780cb11
+# ╠═ee01b25e-c32e-4f6e-96d6-cb9c6f3ea95c
 # ╠═130fca42-cee8-4d88-a764-cdded04a636e
 # ╠═34efe422-e672-4d17-a558-ce32fb704a8e
 # ╠═127c1123-29cf-45a7-b7ab-7f57b4d6e58f
@@ -287,5 +377,9 @@ snap.velocities * lguys.V0
 # ╟─2e7c1798-4066-4c46-b5ed-732263728ac0
 # ╠═f6b27164-ee7c-428b-aefb-75e89d178f3e
 # ╠═5fdd8307-d528-4cd7-a5e4-1f15aba75cd5
-# ╠═9c82b9ce-57a5-4b16-8bd9-6458931ef6f5
-# ╠═89357b44-82e3-4f84-9cf1-3c0c01d1cf03
+# ╠═2dfe9a85-6553-4632-81e0-33c148fd1102
+# ╠═5f45e7c7-e447-48bf-ade4-38f516df2dad
+# ╠═519a88f0-8e2d-4c09-83e0-3cc2ee147e35
+# ╠═5316884b-3971-4ca7-9106-f638241d3388
+# ╠═34b812d2-21c0-4983-9d14-7dbef08ab670
+# ╠═de1e5245-0946-47cd-8e2c-ba1914cfeb74
--- notebooks/orbits.jl
+++ notebooks/orbits.jl
@@ -4,16 +4,6 @@
 using Markdown
 using InteractiveUtils
 
-# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
-macro bind(def, element)
-    quote
-        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
-        local el = $(esc(element))
-        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
-        el
-    end
-end
-
 # ╔═╡ 1f568548-84e8-11ee-0abf-cd4651e92589
 begin
 	import Pkg
@@ -45,35 +35,22 @@ begin
 	savefig("v_circ.pdf")
 end
 
-# ╔═╡ ffa7eb1e-b515-4bf0-9f68-c8ea1c5191c8
-begin
-c = 9.545
-Mhalo = 115 * 1/(log(1+c) - (c)/(1+c))
+# ╔═╡ 08fde7d1-b529-4a37-a77f-c27ea3bdc30d
+begin 
+	ddist = 0 #6.151974
+	dpm_ra = 0# -0.0123
+	dpm_dec = 0 #-0.04785
+	drv = 0# 1.8
 end
-
-# ╔═╡ fc6cc98e-58c9-46e1-8a77-874cb078614d
-σ_slider = Slider(-3:0.1:3, default=0) # a 3 σ slider for standard normal distributions...
-
-# ╔═╡ 27c9d926-06ff-45b1-895e-a93e12749452
-@bind dpm_dec σ_slider
-
-# ╔═╡ a8af2878-70df-4eca-a535-e0b3dd720a81
-@bind dpm_ra σ_slider
-
-# ╔═╡ ea6a411f-428e-4aed-98a8-c2fdf783cfca
-@bind ddistance σ_slider
-
-# ╔═╡ a1e19b57-4cfc-4081-993d-8ad6ca7426de
-@bind dradial_velocity σ_slider
 
 # ╔═╡ aeaae20c-4ee7-4f66-8b1f-52d3afdda205
 begin
-	distance = 86 + 6* ddistance
-	pm_ra =(0.099 + 0.002*dpm_ra)
-	pm_dec = (-0.160 + 0.002*dpm_dec)
-	radial_velocity = (111.4 +0.37 * dradial_velocity)
-	ra=15.03917
-	dec=-33.70917
+	distance = 86 +  ddist
+	pm_ra = 0.099 + dpm_ra
+	pm_dec = -0.160 + dpm_dec
+	radial_velocity = 111.4 + drv
+	ra = 15.03917
+	dec = -33.70917
 	obs = lguys.Observation(ra, dec, distance, pm_ra, pm_dec, radial_velocity)
 
 end
@@ -84,31 +61,18 @@ begin
 	phase2 = lguys.Galactocentric(phase.position, -phase.velocity)
 end
 
-# ╔═╡ 98bb6eb8-ead1-456d-b906-612763a2ddd6
-md"""
-- pm-ra = $(pm_ra)
-- pm-dec = $(pm_dec)
-- rv = $(radial_velocity)
-- distance = $(distance)
-"""
-
-# ╔═╡ 05ad80bc-7892-4c0c-b843-f6b84da2fcc3
-import LinearAlgebra: transpose!
-
 # ╔═╡ 0cbbba9a-8009-4e88-a19d-41fb121ca994
 begin
 	t, positions, velocities, phi = calc_orbit(phase2)
-	t2, positions2, velocities2, phi2 = calc_orbit(phase)
-	t = [a[1] for a in t]
+	t = t.value
+	positions = reverse(positions, dims=2)
+	velocities = -reverse(velocities, dims=2)
 end
 
 # ╔═╡ 3aee96d3-e86f-4b1a-b228-68cfc2c48729
 begin 
 	r = lguys.calc_r(positions)
 	v = lguys.calc_r(velocities)
-
-	r2 = lguys.calc_r(positions2)
-	v2 = lguys.calc_r(velocities2)
 end
 
 # ╔═╡ ccfc0c3c-008e-4cc4-8f90-1f319b8559d3
@@ -119,23 +83,11 @@ The current pericenter is $(minimum(r)) and apocenter $(maximum(r))
 # ╔═╡ 9e8b5a2f-8033-4123-a0fc-78e43c78603b
 begin
 	plot(t, r)
-
-	plot!(-t2.value, r2)
 	hline!([minimum(r), maximum(r)])
 end
 
-# ╔═╡ 0de9032e-e2d3-4cca-925f-c7686f3487e3
-
-begin
-plot(r, v)
-plot!(r2, v2)
-end
-
-# ╔═╡ 73b03786-b61c-4a13-8260-b85f9ad1857b
-lguys.scatter_xyz(positions[:, 1:2], reshape(phase.position, 3, 1))
-
-# ╔═╡ cbc81993-5d73-4831-91e0-ca7039293f4a
-lguys.scatter_xyz(velocities[:, 1:2], reshape(phase.velocity, 3, 1))
+# ╔═╡ 50a2c268-d886-4d8b-b79e-6a204a2f0d98
+phase_i = lguys.Galactocentric(positions[:, 1], velocities[:, 1])
 
 # ╔═╡ f742c296-b2c6-4fd8-8d87-a0ad84b3b525
 begin
@@ -147,27 +99,45 @@ end
 # ╔═╡ f1d9d39b-4d93-4359-9742-59ef67a531e3
 lguys.plot_xyz(positions)
 
+# ╔═╡ 8d23a56a-f90e-4b62-94d4-b4d3609542da
+lguys.plot_xyz(velocities)
+
+# ╔═╡ 0de9032e-e2d3-4cca-925f-c7686f3487e3
+
+begin
+plot(r, v)
+end
+
+# ╔═╡ 2b4696b8-05f6-489b-b8fb-49f5529f4abc
+positions[:, 1]
+
+# ╔═╡ dc63b947-1d17-44b5-bec4-3a0b6218e176
+positions[:, end]
+
+# ╔═╡ 899c01ec-4d13-4b32-9bcf-390e1dcd72dd
+velocities[:, 1]  ./ lguys.V0
+
+# ╔═╡ f804f503-afcb-49b9-a1c2-aad84089290f
+velocities[:, end]  ./ lguys.V0
+
 # ╔═╡ Cell order:
 # ╟─3a360b8d-e830-47ab-bfbc-ce147b15d09d
 # ╠═1f568548-84e8-11ee-0abf-cd4651e92589
 # ╠═553b9e95-2748-4651-b108-0e177b286322
-# ╠═6607d695-788a-4667-91ec-b1d290fb9ea5
-# ╠═ffa7eb1e-b515-4bf0-9f68-c8ea1c5191c8
-# ╠═fc6cc98e-58c9-46e1-8a77-874cb078614d
+# ╟─6607d695-788a-4667-91ec-b1d290fb9ea5
+# ╠═08fde7d1-b529-4a37-a77f-c27ea3bdc30d
 # ╠═aeaae20c-4ee7-4f66-8b1f-52d3afdda205
 # ╠═74eb54e2-77b3-4fed-9408-65547b7bb295
-# ╟─98bb6eb8-ead1-456d-b906-612763a2ddd6
-# ╠═27c9d926-06ff-45b1-895e-a93e12749452
-# ╠═a8af2878-70df-4eca-a535-e0b3dd720a81
-# ╠═ea6a411f-428e-4aed-98a8-c2fdf783cfca
-# ╠═a1e19b57-4cfc-4081-993d-8ad6ca7426de
 # ╟─ccfc0c3c-008e-4cc4-8f90-1f319b8559d3
-# ╠═05ad80bc-7892-4c0c-b843-f6b84da2fcc3
 # ╠═0cbbba9a-8009-4e88-a19d-41fb121ca994
 # ╠═3aee96d3-e86f-4b1a-b228-68cfc2c48729
 # ╠═9e8b5a2f-8033-4123-a0fc-78e43c78603b
-# ╠═0de9032e-e2d3-4cca-925f-c7686f3487e3
-# ╠═73b03786-b61c-4a13-8260-b85f9ad1857b
-# ╠═cbc81993-5d73-4831-91e0-ca7039293f4a
+# ╠═50a2c268-d886-4d8b-b79e-6a204a2f0d98
 # ╠═f742c296-b2c6-4fd8-8d87-a0ad84b3b525
 # ╠═f1d9d39b-4d93-4359-9742-59ef67a531e3
+# ╠═8d23a56a-f90e-4b62-94d4-b4d3609542da
+# ╠═0de9032e-e2d3-4cca-925f-c7686f3487e3
+# ╠═2b4696b8-05f6-489b-b8fb-49f5529f4abc
+# ╠═dc63b947-1d17-44b5-bec4-3a0b6218e176
+# ╠═899c01ec-4d13-4b32-9bcf-390e1dcd72dd
+# ╠═f804f503-afcb-49b9-a1c2-aad84089290f
