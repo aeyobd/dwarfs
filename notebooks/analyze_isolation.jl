53ced46a8e88270891f87262666d5aa0cb6439c4
aeyobd <aeyobd@gmail.com>
2024-04-24 23:20:00 +0100

    more organized codes, exact reproduction of rapha's method

--- notebooks/analyze_isolation.jl
+++ notebooks/analyze_isolation.jl
@@ -21,15 +21,15 @@ begin
 	import NaNMath as nm
 end
 
+# ╔═╡ 9e9d463b-feaa-41bd-96c7-9c320d933b71
+using QuadGK
+
 # ╔═╡ 9f75b286-b021-4fa1-a29d-7051c55c0a33
 if !isdefined(Main, :PlutoRunner) # if running from file
 	using ArgParse
 	println("running as script")
 	dirname2 = get_args()
 end
-
-# ╔═╡ c0df53fd-35d1-48aa-b1ba-131087be3aed
-
 
 # ╔═╡ b7ef1dbd-1865-4ac3-a4d7-26fc9b443c45
 md"""
@@ -39,8 +39,22 @@ To make this both a cml utility and interactive, we take inputs in the following
 # ╔═╡ 82c76c56-e874-4eba-9367-569b656155a2
 pwd()
 
+# ╔═╡ 7eb3e35f-c2a5-499e-b884-85fb59060ec5
+md"""
+# Inputs
+"""
+
 # ╔═╡ 405c2a84-cfaf-469f-8eaa-0765f30a21de
-dirname1 = "/cosma/home/durham/dc-boye1/data/dwarfs/models/sculptor/isolation/1e4/fiducial"
+dirname1 = "/cosma/home/durham/dc-boye1/data/dwarfs/models/sculptor/isolation/1e6"
+
+# ╔═╡ 79b07d75-fb05-4833-ac2c-ea0e9c24e791
+r_s_s = 0.109 # stellar scale radius
+
+# ╔═╡ ef3d1d5f-0979-44a7-8f0f-bf4638ea5612
+ρ_s(r) = 1/8π /r_s_s^3 * exp(-r / r_s_s)
+
+# ╔═╡ 199c29c1-7325-41e2-91e8-854dba902930
+quadgk(r->4π*r^2 * ρ_s(r), 0, Inf)
 
 # ╔═╡ f645be76-6477-4970-b655-603d700a10e7
 begin 
@@ -85,25 +99,43 @@ begin
 	println("loading model from $dirname")
 	out = lguys.Output("out/combined.hdf5")
 
-	cens = CSV.read("data/centres.csv", DataFrames.DataFrame)
+	cens = CSV.read("out/centres.csv", DataFrames.DataFrame)
 	x_cen = transpose(Matrix(cens[:, ["x", "y", "z"]]))
 	v_cen = transpose(Matrix(cens[:, ["vx", "vy", "vz"]]))
 end
 
+# ╔═╡ 97e98ab8-b60b-4b48-b465-a34a16858f88
+md"""
+# Initial/Final
+"""
+
 # ╔═╡ c5672da9-0dad-4d22-abe5-9e186ccde02d
 begin
 	snap_i = out[1]
-	snap_f = out[end]
+	snap_i.x_cen = x_cen[:, 1]
+	snap_i.v_cen = v_cen[:, 1]
+
+	snap_f = out[end - 1]
+	snap_f.x_cen = x_cen[:, end]
+	snap_f.v_cen = v_cen[:, end]
+
 end
 
 # ╔═╡ 0e89851e-763f-495b-b677-b664501a17ef
+# ╠═╡ disabled = true
+#=╠═╡
 begin 
-	scatter(log10.(prof_i.rs), prof_i.Vs_circ * lguys.V0)
-	scatter!(log10.(prof_f.rs), prof_f.Vs_circ * lguys.V0)
+	plot()
 
+	rc, Vc = lguys.calc_V_circ(snap_i)
+	scatter!(log10.(rc), Vc * lguys.V0, label="initial")
+
+	rc, Vc = lguys.calc_V_circ(snap_f)
+	scatter!(log10.(rc), Vc * lguys.V0, label="final")
 	xlabel!("log r")
 	ylabel!("V_circ")
 end
+  ╠═╡ =#
 
 # ╔═╡ 72dfab8a-c6c8-4dcc-b399-a0cf6cb0dea0
 begin 
@@ -125,21 +157,8 @@ histogram2d(snap_i.positions[1, :], snap_i.positions[2, :], bins = LinRange(-10,
 # ╔═╡ 528d0258-c218-4e7a-aa18-f096fdf45270
 histogram2d(snap_f.positions[1, :] .- x_cen[1, end:end], snap_f.positions[2, :] .- x_cen[2, end:end], bins = LinRange(-10, 10, 100))
 
-# ╔═╡ 665ae91c-585c-4561-8f20-7541370cb837
-# ╠═╡ disabled = true
-#=╠═╡
-begin 
-	ps = lguys.extract(snap_i, :positions, p_filt)
-	histogram2d(ps[1, :], ps[2, :], weights=probabilities)
-end
-  ╠═╡ =#
-
-# ╔═╡ 106cbda4-57e0-459b-868b-b44339c944fc
-begin 
-	ps = lguys.extract_vector(snap_f, :positions)
-	radii = lguys.calc_r(ps)
-
-end
+# ╔═╡ 29391db0-a45e-4edb-9c5c-7cfc8c141551
+length(probabilities)
 
 # ╔═╡ 9f6aa667-2478-4e96-9289-4d2c74bbf1c0
 lguys.calc_ρ_hist(radii, weights=probabilities)
@@ -179,19 +198,35 @@ end
 
 # ╔═╡ a8de1562-6afe-4002-b7bd-9d5541e8d354
 begin 
-	plot(xlim=(-2, 2), ylim=(-15, 5), title="stars")
+	plot(xlim=(-2, 2), ylim=(-12, 3), title="stars")
 	plot_ρ_s!(snap_i, label="initial")
 	plot_ρ_s!(snap_f, label="final")
-	vline!([log10(0.9)], label="r_H")
+	vline!([log10(r_s_s)], label="r_s")
+
+	log_r_pred = LinRange(-2, 2, 1000)
+	ρ_s_pred = ρ_s.(10 .^ log_r_pred)
+
+	plot!(log_r_pred, log10.(ρ_s_pred), label="expected")
 
 end
+
+# ╔═╡ e33d56a7-7a0e-4fa9-8f0d-041b43584d59
+sum(lguys.calc_r(snap_i) .< r_s_s)
+
+# ╔═╡ 34d9fdea-8961-44ca-a92f-2f48a281f2cd
+histogram(log10.(lguys.calc_r(snap_i)), ylabel="count", xlabel="log r / kpc", label="")
+
+# ╔═╡ fb0dec74-aaab-43a4-9b37-d13634c5dcad
+md"""
+# Time variation
+"""
 
 # ╔═╡ ebdd5430-c7c1-4fc7-82f5-8acd8ca99070
 begin 
 	Ls = hcat([lguys.calc_L_tot(snap) for snap in out]...)
 	plot(transpose(Ls))
 	xlabel!("snapshot")
-	ylabel!("L/L_0")
+	ylabel!("L")
 end
 
 # ╔═╡ bfa7593c-4915-4e03-83e6-8f790de4c1a5
@@ -240,12 +275,6 @@ begin
 
 end
 
-# ╔═╡ f6e638bc-d777-4ead-bd91-ea8ff1ca67c6
-length(probabilities)
-
-# ╔═╡ 5bc0ee18-076a-4876-bbc1-490d50f20cb9
-
-
 # ╔═╡ f21cfe22-95f3-485d-902b-b022a41548c2
 begin 
 	plot(legend_position=:outertopright)
@@ -266,6 +295,9 @@ begin
 	xlabel!("snapshot")
 	ylabel!("log r contating stellar mass")
 end
+
+# ╔═╡ 967136d3-8d58-4fdc-9537-aa3a85a92528
+out.times * lguys.T0
 
 # ╔═╡ 470466ff-3709-4cdb-93c0-b5b7848a940d
 begin 
@@ -300,19 +332,40 @@ end
 # ╔═╡ a9fe4a34-f8ce-46bd-84fb-bdae17a508a4
 histogram(probabilities, bins=20, yscale=:log10, xlabel="probability", ylabel="count")
 
+# ╔═╡ 665ae91c-585c-4561-8f20-7541370cb837
+# ╠═╡ disabled = true
+#=╠═╡
+begin 
+	ps = lguys.extract(snap_i, :positions, p_filt)
+	histogram2d(ps[1, :], ps[2, :], weights=probabilities)
+end
+  ╠═╡ =#
+
+# ╔═╡ 106cbda4-57e0-459b-868b-b44339c944fc
+begin 
+	ps = lguys.extract_vector(snap_f, :positions)
+	radii = lguys.calc_r(ps)
+
+end
+
 # ╔═╡ Cell order:
 # ╠═6e08e538-bc82-11ee-1a75-d97f506d18c5
 # ╠═374489bc-627f-4fc9-9734-7c49456710ac
-# ╠═c0df53fd-35d1-48aa-b1ba-131087be3aed
 # ╟─b7ef1dbd-1865-4ac3-a4d7-26fc9b443c45
 # ╠═82c76c56-e874-4eba-9367-569b656155a2
-# ╠═9f75b286-b021-4fa1-a29d-7051c55c0a33
+# ╟─7eb3e35f-c2a5-499e-b884-85fb59060ec5
 # ╠═405c2a84-cfaf-469f-8eaa-0765f30a21de
-# ╠═f645be76-6477-4970-b655-603d700a10e7
+# ╠═79b07d75-fb05-4833-ac2c-ea0e9c24e791
+# ╠═ef3d1d5f-0979-44a7-8f0f-bf4638ea5612
+# ╠═9e9d463b-feaa-41bd-96c7-9c320d933b71
+# ╠═199c29c1-7325-41e2-91e8-854dba902930
 # ╠═5717c18c-69cd-4740-a37a-d7ef80d68ae9
+# ╠═f645be76-6477-4970-b655-603d700a10e7
+# ╠═9f75b286-b021-4fa1-a29d-7051c55c0a33
 # ╠═5f7e3de9-a7fe-4217-a53c-0101d4b6314d
 # ╠═2fb86841-fc96-47e1-af57-898fa2690ff3
 # ╠═9104ed25-9bc8-4582-995b-37595b539281
+# ╟─97e98ab8-b60b-4b48-b465-a34a16858f88
 # ╠═c5672da9-0dad-4d22-abe5-9e186ccde02d
 # ╠═0e89851e-763f-495b-b677-b664501a17ef
 # ╠═72dfab8a-c6c8-4dcc-b399-a0cf6cb0dea0
@@ -321,22 +374,25 @@ histogram(probabilities, bins=20, yscale=:log10, xlabel="probability", ylabel="c
 # ╠═528d0258-c218-4e7a-aa18-f096fdf45270
 # ╠═665ae91c-585c-4561-8f20-7541370cb837
 # ╠═106cbda4-57e0-459b-868b-b44339c944fc
+# ╠═29391db0-a45e-4edb-9c5c-7cfc8c141551
 # ╠═9f6aa667-2478-4e96-9289-4d2c74bbf1c0
 # ╠═e5b9ce74-4d2d-4c5d-ad45-b6e233a4ec50
 # ╠═dd3a3e04-7b33-4f84-a935-0860283eca80
 # ╠═27f8deff-96ae-4d9a-a110-d146ac34965a
 # ╠═60f8d0cd-ca8e-457c-98b9-1ee23645f9dd
 # ╠═a8de1562-6afe-4002-b7bd-9d5541e8d354
+# ╠═e33d56a7-7a0e-4fa9-8f0d-041b43584d59
+# ╠═34d9fdea-8961-44ca-a92f-2f48a281f2cd
+# ╟─fb0dec74-aaab-43a4-9b37-d13634c5dcad
 # ╠═ebdd5430-c7c1-4fc7-82f5-8acd8ca99070
 # ╠═bfa7593c-4915-4e03-83e6-8f790de4c1a5
 # ╠═fe476f91-5be3-4cf9-88df-55d9568ab88f
 # ╠═70f9aa47-956c-4db3-ad3a-f18a18318d33
 # ╠═e61c095e-a763-466b-b419-755fd0aadd0d
 # ╠═dc221349-eb61-4ace-8de3-a6c50249aca0
-# ╠═f6e638bc-d777-4ead-bd91-ea8ff1ca67c6
-# ╠═5bc0ee18-076a-4876-bbc1-490d50f20cb9
 # ╠═f21cfe22-95f3-485d-902b-b022a41548c2
 # ╠═38aeb93b-8b79-4880-b0d1-cef180d13bc3
+# ╠═967136d3-8d58-4fdc-9537-aa3a85a92528
 # ╠═470466ff-3709-4cdb-93c0-b5b7848a940d
 # ╠═3b2bb553-0130-4c8a-80ad-6e1f7071a293
 # ╠═ce8420db-b395-4db2-acb2-d900281add38
--- notebooks/stars_rapha.jl
+++ notebooks/stars_rapha.jl
@@ -4,16 +4,6 @@
 using Markdown
 using InteractiveUtils
 
-# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
-macro bind(def, element)
-    quote
-        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
-        local el = $(esc(element))
-        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
-        el
-    end
-end
-
 # ╔═╡ a893932c-f184-42bc-9a0e-0960f10520aa
 begin
 	import Pkg
@@ -33,27 +23,43 @@ begin
 	using PlutoUI
 end
 
+# ╔═╡ 17ffde4b-5796-4915-9741-d594cf0c5ca7
+md"""
+# Daniel's script for painting stars
+based on Rapha's codes. 
+Eddington inversion
+
+Given the directory, the script changes to there and works there
+"""
+
+# ╔═╡ 93045024-a91d-4b31-9a5a-7c999afdb9ec
+md"""
+# Inputs
+"""
+
 # ╔═╡ 7809e324-ba5f-4520-b6e4-c7727c227154
-dirname1 = "/cosma/home/durham/dc-boye1/data/dwarfs/models/sculptor/isolation/1e4/fiducial"
+dirname1 = "/cosma/home/durham/dc-boye1/data/dwarfs/models/sculptor/isolation/1e5/"
 
 # ╔═╡ 855fd729-22b6-4845-9d2b-e796d4a15811
 begin 
 	# parameters 
-	snapname = "./out/snapshot_000.hdf5"
+	idx_i = 5
+	idx_s = lpad(idx_i - 1,3,"0")
+	snapname = "./out/snapshot_$idx_s.hdf5"
 	outname = "./star_probabilities.hdf5"
 	
-	r_s_s = 0.91 # kpc
+	r_s_s = 0.109 # kpc
 	
-	ρ_s(r) = exp((-r/r_s_s))
+	ρ_s(r) = exp(-r/r_s_s)
 	
-	Nr = 50
-	NE = 100
-end
+	Nr = 99
 
-# ╔═╡ 10569544-637d-4ab3-b193-c9b7bf7b3f3e
-md"""
-overwrite $outname ? $(@bind overwrite CheckBox())
-"""
+	# number of energy bins, should not actually 
+	# affect results, only changes the density f(E) is evaluated at
+	NE = 100
+
+	overwrite = true
+end
 
 # ╔═╡ 92cc30ae-0b0a-4a72-aa0c-29150eeee5e0
 begin 
@@ -71,36 +77,51 @@ end
 begin 
 	println("$dirname")
 	snap = lguys.Snapshot(snapname)
-	cens = CSV.read("data/centres.csv", DataFrame)
+	cens = CSV.read("out/centres.csv", DataFrame)
 end
+
+# ╔═╡ e5551ea6-7681-4544-a610-ddb5ee0add7b
+cens[idx_i, :]
+
+# ╔═╡ d7d83daf-6c70-4026-afa7-1f426ee805fa
+cens[idx_i, :].t * lguys.T0
 
 # ╔═╡ 0f5671e6-deb4-11ee-3178-4d3f920f23a2
 begin
-	cen_i = cens[1, :]
+	# centre snapshot
+	cen_i = cens[idx_i, :]
 	snap_i = lguys.copy(snap)
-	snap_i.positions .-= [cen_i.x, cen_i.y, cen_i.z]
-	snap_i.velocities .-= [cen_i.vx, cen_i.vy, cen_i.vz]
+	snap_i.positions .-= 0 * [cen_i.x, cen_i.y, cen_i.z]
+	snap_i.velocities .-= 0 * [cen_i.vx, cen_i.vy, cen_i.vz]
 
 
+	# sort by radii
 	radii = lguys.calc_r(snap_i.positions)
+
 	snap_i = snap_i[sortperm(radii)]
 	radii = sort(radii)
 	vels = lguys.calc_r(snap_i.velocities)
-	
+
+	# calculate energies
 	Φs = lguys.calc_radial_discrete_Φ(snap_i.masses, radii)
 	ke = @. 1/2 * vels^2
 	ϵs = @. -Φs - ke
+
+	# filter snapshot
 	idx = ϵs .> 0
+	idx_excluded = snap_i.index[map(!, idx)]
 	
 	snap_i = snap_i[idx]
-	ke = ke[idx]
 	radii = radii[idx]
 	vels = vels[idx]
+	
+	ke = ke[idx]
 	ϵs = ϵs[idx]
 	Φs = Φs[idx]
-
-	Mins = cumsum(snap_i.masses) ./ sum(snap_i.masses)
 end
+
+# ╔═╡ f79414b4-620e-440e-a421-7bc13d373546
+Mins = cumsum(snap_i.masses) ./ sum(snap_i.masses)
 
 # ╔═╡ 23158c79-d35c-411a-a35a-950f04214e19
 begin 
@@ -108,18 +129,25 @@ begin
 	M_s(r) = 4π * quadgk(r-> r^2 * ρ_s(r) / M_s_tot, 0, r)[1]
 end
 
-# ╔═╡ 7a39cd4f-9646-4969-9410-b093bca633cb
-begin 
+# ╔═╡ 36b4adbd-d706-4e72-a922-53080c67946c
+begin
 	log_radii = log10.(radii)
 	r_min = radii[1]
 	r_max = radii[end]
 	r_e = 10 .^ LinRange(log10.(r_min), log10.(r_max), Nr+1)
 	r = lguys.midpoint(r_e)
+end
+
+# ╔═╡ e37bf6d7-9445-49bf-8333-f68ad25436b2
+r_e
+
+# ╔═╡ 7a39cd4f-9646-4969-9410-b093bca633cb
+begin 
+	# just a informative note
 	N_s_out = 1 - M_s(r_max)
 	N_s_in = M_s(r_min)
-	print("missing $N_s_out stars outside")
-	print("missing $N_s_in stars inside")
-
+	println("missing $N_s_out stars outside")
+	println("missing $N_s_in stars inside")
 end
 
 # ╔═╡ bd1bca1d-0982-47d8-823e-eadc05191b88
@@ -151,19 +179,29 @@ begin
 	f_s = lguys.calc_fϵ(ν_s, ψ_i, r)
 end
 
+# ╔═╡ 126c6825-723f-4d13-b5a3-64cba72fc867
+md"""
+The density distribution of the stars (analytic) and dark matter (calculated) from the snapsho
+"""
+
 # ╔═╡ 8bb8736d-a41b-4dac-a6cd-06d0d4704654
 begin 
-	plot(xlims=(-1.2, 3), ylims=(-15, 0),
+	plot(xlims=(-1.2, 3), ylims=(-15, 2),
 	xlabel="log r", ylabel="log density")
 	plot!(log10.(r), log10.(ν_dm), label="DM")
 	plot!(log10.(r), log10.(ν_s), label="stars (analytic)")
 end
 
+# ╔═╡ 7481f47a-2b8a-45a3-9b4e-31ea14e9d331
+md"""
+The potential $\psi = -\Phi$ as a function of log radii (for the spherically calculated & interpolated and actual snapshot from Gadget 4)
+"""
+
 # ╔═╡ b625d8a5-7265-4849-9bd6-ca8064d392eb
 begin 
 	plot(xlabel="log r / kpc", ylabel="ψ(r)")
 	plot!(log10.(r), ψ, label="interpolated")
-	plot!(log10.(radii), -Φs, label="snapshot")
+	plot!(log10.(radii), -snap_i.Φs, label="snapshot")
 end
 
 # ╔═╡ 78ce5a98-fd3f-4e39-981f-2bea58b117bf
@@ -175,9 +213,14 @@ begin
 	f_s_e = f_s.(E)
 end
 
+# ╔═╡ 8b66d00d-529b-4e8c-9386-b17117996579
+md"""
+The calculated distribution function as a function of log specific binding energy $\epsilon =  -\Phi - T$
+"""
+
 # ╔═╡ 75d23b44-71e7-4e28-ad3e-c537f3d4422f
 begin
-	plot(xlabel="log ϵ", ylabel="log f", ylims=(-15, 5))
+	plot(xlabel="log ϵ", ylabel="log f", ylims=(-15, 7))
 	plot!(log10.(E), nm.log10.(f_dm_e), label="DM")
 	plot!(log10.(E), nm.log10.(f_s_e), label="stars")
 end
@@ -185,31 +228,47 @@ end
 # ╔═╡ 7409a024-cea4-47a5-84d2-846c96d88b7a
 begin 
 	probs = f_s_e ./ f_dm_e
-	probs ./= sum(probs) # arbitrary scaling...
+	probs ./= sum(probs .* lguys.gradient(E)) # pdf, dN/dE
 	prob = lguys.lerp(E, probs)
 end
 
-# ╔═╡ 84fdc265-988c-40db-87e5-44ba55d0e412
-begin 
-	plot(xlabel="log ϵ", ylabel="f_s / f_dm")
-	plot!(log10.(E), f_s_e ./ f_dm_e / 50, label="probability")
-	stephist!(log10.(ϵs), norm=:pdf, label="dark matter")
-end
+# ╔═╡ a389fde4-f05f-48ff-9a8c-8eb0643a849d
+ϵs
 
 # ╔═╡ 3b229c8e-9320-4c07-b948-c34a0c082341
 begin 
 	ps = prob.(ϵs)
 	print(sum(ps .< 0), " negative probabilities")
 	ps[ps .< 0] .= 0
+	ps[isnan.(ps)] .= 0
 	ps ./= sum(ps)
 end
+
+# ╔═╡ 84fdc265-988c-40db-87e5-44ba55d0e412
+begin 
+	plot(xlabel="ϵ (binding energy)", ylabel="relative density", legend_position=:outertopright, size=(600, 300))
+	plot!((E), 20*prob.(E) ./maximum(prob.(E)), label="f_s / f_dm")
+	stephist!((ϵs), norm=:pdf, label="dark matter")
+	stephist!((ϵs), weights=100ps, label="stars (nbody)")
+	vline!([maximum(ϵs)], color="grey", ls=:dot, label=L"\epsilon_{\rm max}")
+end
+
+# ╔═╡ daf54cb4-06a3-4a8e-a533-354ca8740aec
+md"""
+A histogram of the assigned (positive) stellar weights. See the number of negative probabilities above
+"""
 
 # ╔═╡ 77e2c1e3-7756-4ab7-810a-03ccdc635aa1
 begin 
 	plot(xlabel="stellar weights", ylabel="frequency")
-
-	histogram!((ps), yscale=:log10)
+	histogram!(ps, nbins=100, yscale=:log10)
 end
+
+# ╔═╡ 5b9c7242-5d4b-4f5a-83e5-3e4d11017aa5
+sum(ps .> 0)
+
+# ╔═╡ b1a34b9b-c6ca-4818-95b2-5b55fb32511e
+sum(ps .== 0)
 
 # ╔═╡ a5bc5ce3-8e33-4514-bc2d-4b4299f104f9
 begin 
@@ -246,88 +305,151 @@ begin
 	ν_s_nbody = lguys.calc_ρ_hist(radii, r_e, weights=ps)[2]
 end
 
+# ╔═╡ b67a17dc-7a59-4b62-9120-4c2ada12298c
+md"""
+The main result. The reconstructed density profile
+"""
+
 # ╔═╡ a9335e17-a410-455a-9a9e-d63706a026bd
 begin
-	plot(xlabel=L"\log  \rm r / kpc", ylabel=L"\log \nu", ylim=(-10, 0), xlim=(-1.5, 2.3), fontfamily="Times")
+	p_nu_1 = plot(ylabel=L"\log \nu", ylim=(-4.8, 1.2), xlim=(-1.2, 0.3), fontfamily="Times")
 	plot!(log10.(r), nm.log10.(ν_s), label="stars")
 	plot!(log10.(r) , nm.log10.(ν_s_nbody), label="nbody")
-end
 
-# ╔═╡ b688d4f8-7a24-4ca0-a0e9-5b08c5b304e3
-begin
-	plot(xlabel=L"\log  \rm r / kpc", ylabel=L"\log \nu - \log \nu_{\rm analytic}", ylim=(-1, 1), size=(600, 200), xlim=(-1.5, 2.3))
+	p_nu_2 = plot(xlabel=L"\log  \rm r / kpc", ylabel=L"\Delta\log \nu ", ylim=(-1, 1), size=(600, 200), xlim=(-2.0, 0.7))
 	scatter!(log10.(r), nm.log10.(ν_s_nbody) .- nm.log10.(ν_s), label="")
 	hline!([0], color="black", label="", z_order=1)
+
+	plot(p_nu_1, p_nu_2, layout=grid(2, 1, heights=[0.8, 0.2]))
 end
 
-# ╔═╡ 27071738-f294-4e48-99f0-a08f2e369137
-lguys.plot_centre(snap_i.positions, marker_z = ps, cmap=cgrad(:greys, rev=true), ms=1.5, width=15)
+# ╔═╡ ec46946a-0bf7-4374-af44-8d8b2ab6a3df
+ps
 
-# ╔═╡ ffec02d2-32ed-4396-b5ff-2021724476d9
-lguys.plot_centre(snap_i.positions, ms=1, width=10)
+# ╔═╡ 06e1b872-ce52-434f-a8d1-3b0a5055eed2
+md"""
+A histogram of the stellar density
+"""
 
-# ╔═╡ 7348b63e-43fa-4ff0-98dd-9de7f3167243
+# ╔═╡ 90856551-fff8-4d15-be66-6353091b5e50
 begin 
-	idx0 = snap.index
-	p_idx = zeros(length(idx0))
-	idx_s = snap_i.index
-	for i in 1:length(idx_s)
-		j = findfirst(isequal(idx_s[i]), idx0 )
-		p_idx[j] = ps[i]
-	end
+	r_hist = 5
+	N_hist = 100
 end
+
+# ╔═╡ cc231e78-cfc0-4876-9f8b-980139f7d27f
+begin 
+	flt = ps .> 0
+	histogram2d(lguys.get_x(snap_i)[flt], lguys.get_y(snap_i)[flt], weights=ps[flt], bins=LinRange(-r_hist, r_hist, N_hist), colorbar_scale=:log10, aspect_ratio=1, clim=(1e-8, 1), xlims=(-r_hist, r_hist))	
+end
+
+# ╔═╡ ee866164-c6f2-4f70-bde1-360abd5fd80e
+md"""
+Histogram of same region in dark matter only (over a smaller dynamic range). Dark matter is much more extended (as expected)
+"""
+
+# ╔═╡ a52e5e94-6068-4545-962f-e02a485b62f5
+begin 
+	histogram2d(lguys.get_x(snap_i), lguys.get_y(snap_i), bins=LinRange(-r_hist, r_hist, N_hist), colorbar_scale=:log10, aspect_ratio=1, xlims=(-r_hist, r_hist))	
+end
+
+# ╔═╡ a3071af2-beff-408d-b109-d4f289f8f7f4
+md"""
+## Last checks and saving profile
+"""
+
+# ╔═╡ a483545a-42ea-474b-a2ca-6f1bd4c7275b
+p_idx = snap_i.index
+
+# ╔═╡ 7bc02274-aa44-4609-b9a6-e409de5172af
+begin
+	idx_all = vcat(p_idx, idx_excluded)
+	ps_all = vcat(ps, zeros(length(idx_excluded)))
+end
+
+# ╔═╡ f66a4a6d-b31c-481b-bf7a-4801c783ceb4
+sort(idx_all) == sort(snap.index) # check we didn't lose any star particles
+
+# ╔═╡ 92798e2e-2356-43c2-a4a9-82a70619a5f5
+sum(ps_all[idx_excluded]) # should be zero
+
+# ╔═╡ 4671b864-470d-4181-993b-4e64d5687460
+sum(ps_all) # should be 1
 
 # ╔═╡ bd8489da-ac53-46c6-979e-06d5dc6e25d1
 function write_stars()
-		if isfile(outname)
-			if overwrite
-				rm(outname)
-			else
-				println("file already exists")
-				return
-			end
+	if isfile(outname)
+		if overwrite
+			rm(outname)
+		else
+			println("file already exists")
+			return
 		end
-		
-		h5write(outname, "/index", idx0)
-		h5write(outname, "/probabilities", p_idx)
-		println("probabilities written to $outname")
+	end
+
+
+	h5write(outname, "/index", idx_all)
+	h5write(outname, "/probabilities", ps_all)
+	println("probabilities written to $outname")
 end
 
 # ╔═╡ f42cb7e1-64b7-47da-be05-dd50c2471fb3
 write_stars()
 
 # ╔═╡ Cell order:
+# ╟─17ffde4b-5796-4915-9741-d594cf0c5ca7
 # ╠═a893932c-f184-42bc-9a0e-0960f10520aa
 # ╠═641946b3-e6f2-4d6d-8777-7698f353eb3d
+# ╟─93045024-a91d-4b31-9a5a-7c999afdb9ec
 # ╠═7809e324-ba5f-4520-b6e4-c7727c227154
 # ╠═855fd729-22b6-4845-9d2b-e796d4a15811
-# ╟─10569544-637d-4ab3-b193-c9b7bf7b3f3e
-# ╠═92cc30ae-0b0a-4a72-aa0c-29150eeee5e0
+# ╟─92cc30ae-0b0a-4a72-aa0c-29150eeee5e0
 # ╠═4cb09115-143d-456f-9c6a-19656f638677
+# ╠═e5551ea6-7681-4544-a610-ddb5ee0add7b
+# ╠═d7d83daf-6c70-4026-afa7-1f426ee805fa
+# ╠═e37bf6d7-9445-49bf-8333-f68ad25436b2
 # ╠═0f5671e6-deb4-11ee-3178-4d3f920f23a2
+# ╠═f79414b4-620e-440e-a421-7bc13d373546
 # ╠═23158c79-d35c-411a-a35a-950f04214e19
+# ╠═36b4adbd-d706-4e72-a922-53080c67946c
 # ╠═7a39cd4f-9646-4969-9410-b093bca633cb
 # ╠═bd1bca1d-0982-47d8-823e-eadc05191b88
 # ╠═1fab14ec-6bfc-4243-bc48-915d1a129925
 # ╠═dfa675d6-aa32-45c3-a16c-626e16f36083
+# ╟─126c6825-723f-4d13-b5a3-64cba72fc867
 # ╠═8bb8736d-a41b-4dac-a6cd-06d0d4704654
+# ╟─7481f47a-2b8a-45a3-9b4e-31ea14e9d331
 # ╠═b625d8a5-7265-4849-9bd6-ca8064d392eb
 # ╠═78ce5a98-fd3f-4e39-981f-2bea58b117bf
+# ╟─8b66d00d-529b-4e8c-9386-b17117996579
 # ╠═75d23b44-71e7-4e28-ad3e-c537f3d4422f
 # ╠═7409a024-cea4-47a5-84d2-846c96d88b7a
 # ╠═84fdc265-988c-40db-87e5-44ba55d0e412
+# ╠═a389fde4-f05f-48ff-9a8c-8eb0643a849d
 # ╠═3b229c8e-9320-4c07-b948-c34a0c082341
+# ╟─daf54cb4-06a3-4a8e-a533-354ca8740aec
 # ╠═77e2c1e3-7756-4ab7-810a-03ccdc635aa1
+# ╠═5b9c7242-5d4b-4f5a-83e5-3e4d11017aa5
+# ╠═b1a34b9b-c6ca-4818-95b2-5b55fb32511e
 # ╠═a5bc5ce3-8e33-4514-bc2d-4b4299f104f9
 # ╠═33a26663-0c08-411b-902b-a509b2afa5ad
 # ╠═a73db457-2cc2-4917-bb25-0429f4daecbd
 # ╠═0e7a922f-4e61-4d02-ac28-3915f5d1c9da
 # ╠═7d69e393-c4db-4ff0-ab5a-85ac50c785c2
 # ╠═6fba7fa7-9a50-4379-b376-5c07f3638411
+# ╟─b67a17dc-7a59-4b62-9120-4c2ada12298c
 # ╠═a9335e17-a410-455a-9a9e-d63706a026bd
-# ╠═b688d4f8-7a24-4ca0-a0e9-5b08c5b304e3
-# ╠═27071738-f294-4e48-99f0-a08f2e369137
-# ╠═ffec02d2-32ed-4396-b5ff-2021724476d9
-# ╠═7348b63e-43fa-4ff0-98dd-9de7f3167243
-# ╠═f42cb7e1-64b7-47da-be05-dd50c2471fb3
+# ╠═ec46946a-0bf7-4374-af44-8d8b2ab6a3df
+# ╟─06e1b872-ce52-434f-a8d1-3b0a5055eed2
+# ╠═90856551-fff8-4d15-be66-6353091b5e50
+# ╠═cc231e78-cfc0-4876-9f8b-980139f7d27f
+# ╟─ee866164-c6f2-4f70-bde1-360abd5fd80e
+# ╠═a52e5e94-6068-4545-962f-e02a485b62f5
+# ╟─a3071af2-beff-408d-b109-d4f289f8f7f4
+# ╠═a483545a-42ea-474b-a2ca-6f1bd4c7275b
+# ╠═7bc02274-aa44-4609-b9a6-e409de5172af
+# ╠═f66a4a6d-b31c-481b-bf7a-4801c783ceb4
+# ╠═92798e2e-2356-43c2-a4a9-82a70619a5f5
+# ╠═4671b864-470d-4181-993b-4e64d5687460
 # ╠═bd8489da-ac53-46c6-979e-06d5dc6e25d1
+# ╠═f42cb7e1-64b7-47da-be05-dd50c2471fb3
