047329ffdd701ceafec5e671c31d3cbc81b74811
aeyobd <aeyobd@gmail.com>
2026-03-10 17:48:46 +0000

    Boo3 velocity analysis

---
+++ observations/bootes3/velocities/clean_deimos.jl
@@ -0,0 +1,390 @@
+### A Pluto.jl notebook ###
+# v0.20.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 04bbc735-e0b4-4f0a-9a83-e50c8b923caf
+begin 
+	import Pkg; Pkg.activate()
+	
+	using CSV, DataFrames
+	using PythonCall
+	using Arya
+	using CairoMakie
+
+	import LilGuys as lguys
+end
+
+# ╔═╡ 2f5ca80c-d98d-45cc-a661-834002b620f6
+using PairPlots
+
+# ╔═╡ 23b3766e-15b7-4b1e-bb41-d6af36a59caf
+using PyFITS
+
+# ╔═╡ 7ed5bcf5-dfc3-4e79-a608-d503124a1e96
+using LilGuys
+
+# ╔═╡ e67ec69b-cfd1-4da7-8f0a-3c1a30f6f48c
+using Turing
+
+# ╔═╡ 811c5da0-7e70-4393-b59d-c0fdb89523ca
+md"""
+# Deimos
+
+This notebook analyzes the RV stars in from Geha+2026's reprocessing of legacy Keck/DEIMOS observations of Boo 3 (from Carlin et al. 2019) and creates a sample sutable to be combined with others for velocity analysis.
+
+"""
+
+# ╔═╡ 9e9ba645-b780-4afa-b305-a2b1d8a97220
+import StatsBase: quantile, mean, std, median, kurtosis, sem
+
+# ╔═╡ 257caa92-ccce-44ff-88ee-6a9c550ae5d9
+CairoMakie.activate!(type=:png)
+
+# ╔═╡ ef1bb6f5-00b8-405b-8702-244eca618644
+import DensityEstimators: histogram, calc_limits, make_bins
+
+# ╔═╡ 36634dea-21bc-4823-8a15-7bce20b6fc17
+import TOML
+
+# ╔═╡ 9a20ce08-79ce-4e23-ac4d-1c0af8de6ea7
+module RVUtils
+	include("../../rv_utils.jl")
+end
+
+# ╔═╡ 3e0eb6d1-6be4-41ec-98a5-5e9167506e61
+data_dir = "../data/"
+
+# ╔═╡ c470c2b9-093d-42ab-b96d-9dac231ccabc
+md"""
+## Data loading
+"""
+
+# ╔═╡ d6a2ebb4-f125-4f28-9009-fc67d5dda5a1
+raw_table = CSV.read("../data/deimos_velocities.txt", DataFrame, ignorerepeated=true, delim=" ", skipto=36, 
+	header=["system", "ra", "dec", "r_mag", "g_r", "nmask", "texp", "SN", "RV",
+			"RV_err", "WECaT", "EWCaT_err", "FE_H", "FE_H_err", "Var", "P_SAT_deimos"]
+					)
+
+# ╔═╡ d5e1fc38-9b93-4024-9259-c9253914c72a
+boo3_deimos = raw_table[raw_table[!, "system"] .== Ref("Boo3"), :]
+
+# ╔═╡ 77e7884c-0360-4b7f-b9ad-e81be2516552
+obs_properties = TOML.parsefile("../observed_properties.toml")
+
+# ╔═╡ 0137b308-f0c6-4e73-afa6-346797b6f304
+j24 = read_fits("$data_dir/j24_2c.fits") 
+
+# ╔═╡ 2c4eabb0-3af9-4b6f-b15d-9ed3b4290040
+xmatch_max = 2.0
+
+# ╔═╡ 0ed1ac33-0537-4838-ae0b-200be73c501c
+md"""
+Depends on:
+- `../data/j24_2c.fits`
+- `../observed_properties.toml`
+- `../data/deimos_velocities.txt`
+Creates:
+- `processed/rv_deimos.fits`
+- `processed/mcmc_summary_uncorrected_spencer+18.csv`
+
+xmatch by nearest star within $xmatch_max arcmin.
+"""
+
+# ╔═╡ d321c481-0ceb-4e3b-b5fc-12af735155e3
+filt_xmatch, idx_xmatch = RVUtils.xmatch(boo3_deimos, j24, xmatch_max)
+
+# ╔═╡ 96e1413d-e4fd-48af-8cde-73a5fcb4976a
+begin 
+	source_id = allowmissing(j24.source_id[idx_xmatch])
+	source_id[.!filt_xmatch] .= missing
+end
+
+# ╔═╡ a6f7f1a1-2dad-4397-9565-7b2aaf1fa733
+F_best = RVUtils.get_f_best.([j24], source_id)
+
+# ╔═╡ 412148d2-3b14-400a-b93b-f4aa18965d8d
+F_match = F_best .=== 1.
+
+# ╔═╡ 28a22929-89f2-422d-9cf0-b06d7e45d9a4
+df_out = let
+	df = copy(boo3_deimos)
+	df[!, :source_id] .= source_id
+	df[!, :F_match] = F_match
+	df[!, :F_scatter] .= true
+	df
+end
+
+# ╔═╡ f71152ad-d576-4205-bece-92c85783c089
+write_fits("processed/rv_deimos.fits", df_out, overwrite=true)
+
+# ╔═╡ 4b8a45ce-4862-4842-8c60-bae9a64acd75
+md"""
+### Sanity checks with sourceid matching
+"""
+
+# ╔═╡ 423e446b-3cf6-4150-8b94-c4d7388fceb4
+@assert length(unique(skipmissing(df_out.source_id))) == sum(filt_xmatch)
+
+# ╔═╡ 011700b8-5dcc-409b-807c-952ea9726992
+df_out[nonunique(df_out, :source_id, keep=:noduplicates), :]
+
+# ╔═╡ 70729597-2e50-4773-89ad-87e97f7bdad6
+length(unique(df_out.source_id)), sum(.!ismissing.(df_out.source_id)), sum(filt_xmatch)
+
+# ╔═╡ 70dbd229-281a-4796-873d-4be2a4000e34
+md"""
+# Numbers
+"""
+
+# ╔═╡ 2612309e-f128-4ddf-96d8-91b86552de17
+length(boo3_deimos.RV)
+
+# ╔═╡ 8064bea8-ff54-4a71-807c-30f52f68f1f1
+length(df_out.RV)
+
+# ╔═╡ 7a48caaa-d953-4d1e-b7db-2d07d8773bf2
+sum(F_match)
+
+# ╔═╡ e11da237-4c8f-450f-bd41-b2c9cdf2f11d
+sum(F_match)
+
+# ╔═╡ 9ac3364e-5532-457e-b29b-bfb10d32e845
+matched = j24[idx_xmatch, :]
+
+# ╔═╡ 7962908c-4150-46a8-8388-2f5283b1d0c4
+matched[matched.F_BEST .== 0.0, :]
+
+# ╔═╡ 34b1fcc3-7355-4d0d-b0aa-8af0541e8651
+mean(matched[matched.F_BEST .== 0.0, :F_CPAR])
+
+# ╔═╡ 08967635-801f-44dc-bb7e-62abbe0f86e4
+mean(matched[matched.F_BEST .== 0.0, :F_INGRID])
+
+# ╔═╡ 1853d75f-65e9-4ad0-8a5e-68603d4ecddf
+mean(matched[matched.F_BEST .== 0.0, :F_ASTROMETRIC])
+
+# ╔═╡ 4290a51e-961b-4b38-b954-1a65e946080c
+md"""
+# Plots
+"""
+
+# ╔═╡ 8a176998-016d-4932-946f-eca78eb53d2b
+md"""
+## Histograms / exporatory
+"""
+
+# ╔═╡ cee182e1-5545-4b86-9152-b288a9b11d86
+hist(boo3_deimos.SN)
+
+# ╔═╡ c0c02bce-e0f9-4fa0-bba6-a5360a825e23
+xmatch_dists = lguys.angular_distance.(df_out.ra, df_out.dec, j24.ra[idx_xmatch], j24.dec[idx_xmatch]) * 60^2
+
+# ╔═╡ d3e682ee-0dab-4aa5-9f3e-423a9d2f92b0
+let
+	fig = Figure()
+	ax = Axis(fig[1,1],
+		xlabel = "distance / arcsec",
+			  yscale=log10,
+			  yticks=Makie.automatic,
+	)
+	hist!(xmatch_dists, bins=0:0.05:3)
+	vlines!(xmatch_max, color=:black )
+	fig
+end
+
+# ╔═╡ c873ccf2-8ecf-4a85-a152-76acd1c1fc62
+let
+	fig = Figure()
+	ax = Axis(fig[1,1],
+		xlabel = "ra",
+			  ylabel = "dec",
+	)
+	scatter!(j24.ra[idx_xmatch[filt_xmatch]], j24.dec[idx_xmatch[filt_xmatch]], markersize=2)
+
+	scatter!(df_out.ra[filt_xmatch], df_out.dec[filt_xmatch], markersize=1,alpha=1, marker=:circ, color=COLORS[2])
+
+	fig
+end
+
+# ╔═╡ fe505b23-c610-4e44-98f4-fb4c9ffe0388
+μ = obs_properties["radial_velocity"]
+
+# ╔═╡ d3440c3e-be84-403f-a04d-68a237375b62
+σ = obs_properties["sigma_v"]
+
+# ╔═╡ 33d34e7e-0c0a-4c49-b0c1-0116c8ef7a38
+filt_memb = μ - 3σ .< boo3_deimos.RV .<  μ + 3σ
+
+# ╔═╡ 98167648-091f-42a1-aac2-70f985f06e09
+filt_memb
+
+# ╔═╡ 344f3c29-0873-4180-9025-51aeaeb2c681
+boo3_members = boo3_deimos[filt_memb, :]
+
+# ╔═╡ 5f71b8e9-5540-4431-8028-4ce14c8d7856
+let 
+	fig, ax = FigAxis()
+	bins = make_bins(boo3_deimos.RV, calc_limits(boo3_deimos.RV), bandwidth=1)
+
+	h = histogram(boo3_deimos.RV, bins, normalization=:none)
+	lines!(h)
+	
+	h = histogram(boo3_members.RV, bins, normalization=:none)
+
+	lines!(h)
+
+
+	fig
+end
+
+# ╔═╡ 214ed7f8-f338-4186-8f6d-870498486c0a
+md"""
+## MCMC
+"""
+
+# ╔═╡ af916274-d081-45dd-ba62-626f7d409ebe
+model = RVUtils.model_vel_1c(boo3_members.RV, boo3_members.RV_err)
+
+# ╔═╡ cd34e5eb-c330-4476-b20a-fe74e67fe69c
+chain = sample(model, NUTS(), MCMCThreads(), 1000, 16)
+
+# ╔═╡ 68fd09a2-d2ca-44a1-b141-94fe9b968ce9
+pairplot(chain)
+
+# ╔═╡ 54c4c8b3-f9e3-4b85-afd6-5a5646f5eb79
+df_summary = RVUtils.summarize(chain)
+
+# ╔═╡ 0ae09680-cc34-492d-85bb-dec3ced936cf
+samples = DataFrame(chain)
+
+# ╔═╡ beb53bbc-0d2a-468d-a40c-2958df7e95ca
+RVUtils.plot_samples(boo3_members, samples, bins=6)
+
+# ╔═╡ 60f45b81-8beb-4eb0-8f55-db04c5368eee
+CSV.write("processed/mcmc_summary_uncorrected_deimos.csv", df_summary)
+
+# ╔═╡ 2cae0f08-20fe-4a83-ad7e-b7d644e6fb27
+md"""
+# Comparing against Carlin data
+"""
+
+# ╔═╡ 1c44bc59-27bc-40c1-a3c9-1148bca74545
+boo3_carlin = CSV.read("../data/carlin_sand2018.txt", DataFrame, ignorerepeated=true, delim=" ")
+
+# ╔═╡ 2bf0545e-9937-4917-9027-6ec864dd698a
+filt_xmatch_carlin, idx_xmatch_carlin = RVUtils.xmatch(boo3_carlin, boo3_deimos, 1)
+
+# ╔═╡ 197703f0-83b6-433a-9a3a-1cf89c8a7393
+let
+	fig = Figure()
+	ax = Axis(fig[1,1],
+			  xlabel = "RV deimos",
+			  ylabel="RV carlin",
+			  aspect=DataAspect()
+			  
+			 )
+
+	x = boo3_deimos[idx_xmatch_carlin[filt_xmatch_carlin], "RV"]
+	y = boo3_carlin[filt_xmatch_carlin, "RV"]
+	x_err = boo3_deimos[idx_xmatch_carlin[filt_xmatch_carlin], "RV_err"]
+	y_err = boo3_carlin[filt_xmatch_carlin, "RV_err"]
+	errorscatter!(x, y, xerror=x_err, yerror=y_err)
+
+	lines!([140, 220], [140, 220])
+	fig
+end
+
+# ╔═╡ f0896659-acbe-48d5-b98b-b889eee4b30b
+boo3_carlin_memb = boo3_carlin[boo3_carlin.member .== Ref("Y"), :]
+
+# ╔═╡ 57b5826e-5cac-4977-9cb5-659b674e9544
+model_carlin = RVUtils.model_vel_1c(boo3_carlin_memb.RV, boo3_carlin_memb.RV_err)
+
+# ╔═╡ d9840d11-997f-4f98-aef5-a061e8befbe0
+chain_carlin = sample(model_carlin, NUTS(), MCMCThreads(), 1000, 16)
+
+# ╔═╡ e8af46ed-8fd7-4725-bbfa-8d942e30bdc2
+pairplot(chain_carlin)
+
+# ╔═╡ 5f4e35e3-fb36-4bf2-a69d-30e1008db2c8
+df_summary_carlin = RVUtils.summarize(chain_carlin)
+
+# ╔═╡ 2f40cf0f-ecc5-4f03-b2a8-43277cc33454
+samples_carlin = DataFrame(chain_carlin)
+
+# ╔═╡ 6b6fcc66-b6ac-4b3f-b877-ddd45092dddd
+RVUtils.plot_samples(boo3_carlin_memb, samples_carlin, bins=5)
+
+# ╔═╡ Cell order:
+# ╠═811c5da0-7e70-4393-b59d-c0fdb89523ca
+# ╠═0ed1ac33-0537-4838-ae0b-200be73c501c
+# ╠═04bbc735-e0b4-4f0a-9a83-e50c8b923caf
+# ╠═9e9ba645-b780-4afa-b305-a2b1d8a97220
+# ╠═2f5ca80c-d98d-45cc-a661-834002b620f6
+# ╠═257caa92-ccce-44ff-88ee-6a9c550ae5d9
+# ╠═ef1bb6f5-00b8-405b-8702-244eca618644
+# ╠═23b3766e-15b7-4b1e-bb41-d6af36a59caf
+# ╠═7ed5bcf5-dfc3-4e79-a608-d503124a1e96
+# ╠═36634dea-21bc-4823-8a15-7bce20b6fc17
+# ╠═e67ec69b-cfd1-4da7-8f0a-3c1a30f6f48c
+# ╠═9a20ce08-79ce-4e23-ac4d-1c0af8de6ea7
+# ╠═3e0eb6d1-6be4-41ec-98a5-5e9167506e61
+# ╟─c470c2b9-093d-42ab-b96d-9dac231ccabc
+# ╠═d6a2ebb4-f125-4f28-9009-fc67d5dda5a1
+# ╠═d5e1fc38-9b93-4024-9259-c9253914c72a
+# ╠═77e7884c-0360-4b7f-b9ad-e81be2516552
+# ╠═0137b308-f0c6-4e73-afa6-346797b6f304
+# ╠═2c4eabb0-3af9-4b6f-b15d-9ed3b4290040
+# ╠═d321c481-0ceb-4e3b-b5fc-12af735155e3
+# ╠═96e1413d-e4fd-48af-8cde-73a5fcb4976a
+# ╠═a6f7f1a1-2dad-4397-9565-7b2aaf1fa733
+# ╠═412148d2-3b14-400a-b93b-f4aa18965d8d
+# ╠═28a22929-89f2-422d-9cf0-b06d7e45d9a4
+# ╠═f71152ad-d576-4205-bece-92c85783c089
+# ╟─4b8a45ce-4862-4842-8c60-bae9a64acd75
+# ╠═423e446b-3cf6-4150-8b94-c4d7388fceb4
+# ╠═011700b8-5dcc-409b-807c-952ea9726992
+# ╠═70729597-2e50-4773-89ad-87e97f7bdad6
+# ╟─70dbd229-281a-4796-873d-4be2a4000e34
+# ╠═2612309e-f128-4ddf-96d8-91b86552de17
+# ╠═8064bea8-ff54-4a71-807c-30f52f68f1f1
+# ╠═7a48caaa-d953-4d1e-b7db-2d07d8773bf2
+# ╠═e11da237-4c8f-450f-bd41-b2c9cdf2f11d
+# ╠═9ac3364e-5532-457e-b29b-bfb10d32e845
+# ╠═7962908c-4150-46a8-8388-2f5283b1d0c4
+# ╠═34b1fcc3-7355-4d0d-b0aa-8af0541e8651
+# ╠═08967635-801f-44dc-bb7e-62abbe0f86e4
+# ╠═1853d75f-65e9-4ad0-8a5e-68603d4ecddf
+# ╟─4290a51e-961b-4b38-b954-1a65e946080c
+# ╟─8a176998-016d-4932-946f-eca78eb53d2b
+# ╠═cee182e1-5545-4b86-9152-b288a9b11d86
+# ╠═c0c02bce-e0f9-4fa0-bba6-a5360a825e23
+# ╠═d3e682ee-0dab-4aa5-9f3e-423a9d2f92b0
+# ╠═c873ccf2-8ecf-4a85-a152-76acd1c1fc62
+# ╠═98167648-091f-42a1-aac2-70f985f06e09
+# ╠═344f3c29-0873-4180-9025-51aeaeb2c681
+# ╠═fe505b23-c610-4e44-98f4-fb4c9ffe0388
+# ╠═d3440c3e-be84-403f-a04d-68a237375b62
+# ╠═33d34e7e-0c0a-4c49-b0c1-0116c8ef7a38
+# ╠═5f71b8e9-5540-4431-8028-4ce14c8d7856
+# ╟─214ed7f8-f338-4186-8f6d-870498486c0a
+# ╠═af916274-d081-45dd-ba62-626f7d409ebe
+# ╠═cd34e5eb-c330-4476-b20a-fe74e67fe69c
+# ╠═68fd09a2-d2ca-44a1-b141-94fe9b968ce9
+# ╠═54c4c8b3-f9e3-4b85-afd6-5a5646f5eb79
+# ╠═0ae09680-cc34-492d-85bb-dec3ced936cf
+# ╠═beb53bbc-0d2a-468d-a40c-2958df7e95ca
+# ╠═60f45b81-8beb-4eb0-8f55-db04c5368eee
+# ╠═2cae0f08-20fe-4a83-ad7e-b7d644e6fb27
+# ╠═1c44bc59-27bc-40c1-a3c9-1148bca74545
+# ╠═2bf0545e-9937-4917-9027-6ec864dd698a
+# ╠═197703f0-83b6-433a-9a3a-1cf89c8a7393
+# ╠═f0896659-acbe-48d5-b98b-b889eee4b30b
+# ╠═57b5826e-5cac-4977-9cb5-659b674e9544
+# ╠═d9840d11-997f-4f98-aef5-a061e8befbe0
+# ╠═e8af46ed-8fd7-4725-bbfa-8d942e30bdc2
+# ╠═5f4e35e3-fb36-4bf2-a69d-30e1008db2c8
+# ╠═2f40cf0f-ecc5-4f03-b2a8-43277cc33454
+# ╠═6b6fcc66-b6ac-4b3f-b877-ddd45092dddd
---
+++ observations/bootes3/velocities/select_members.jl
@@ -0,0 +1,303 @@
+### A Pluto.jl notebook ###
+# v0.20.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 04bbc735-e0b4-4f0a-9a83-e50c8b923caf
+begin 
+	import Pkg; Pkg.activate()
+	using Arya
+	using CairoMakie
+
+	import LilGuys as lguys
+end
+
+# ╔═╡ 93838644-cad6-4df3-b554-208b7afeb3b8
+using PyFITS
+
+# ╔═╡ bd6dfd17-02ee-4855-be37-fecfdab6776f
+using LilGuys; FIGDIR = "figures"
+
+# ╔═╡ 72f1febc-c6ea-449a-8cec-cd0e49c4e20c
+using DataFrames
+
+# ╔═╡ d7d439be-b77b-4a3d-9fb6-7dd7583dc52e
+using OrderedCollections
+
+# ╔═╡ 035cdedb-da23-4cfd-aa19-a3aff089d3ac
+using Measurements
+
+# ╔═╡ 3ed8c28f-5908-42dc-a56b-24a9b2685a07
+md"""
+## Inputs
+"""
+
+# ╔═╡ 50488b8f-6886-4191-8778-af66929f1445
+begin 
+	rv_file = "rv_deimos.fits"
+	j24_sample = "2c"
+	psat_min = 0.2
+end
+
+# ╔═╡ 680e7f76-cb4d-40d6-9a9f-d4672427a633
+md"""
+## derived
+"""
+
+# ╔═╡ 3e0eb6d1-6be4-41ec-98a5-5e9167506e61
+data_dir = "processed"
+
+# ╔═╡ 86fe351f-ef12-474a-85cc-c10c22a65e77
+outname = splitext(basename(rv_file))[1] * "_x_" * j24_sample * "_psat_$psat_min"
+
+# ╔═╡ 7330c75e-1bf9-476a-8274-ebc86d555e6f
+md"""
+# RV sample models
+"""
+
+# ╔═╡ 6bec7416-40c8-4e2b-9d3d-14aa19e5642d
+md"""
+This notebook takes the dataset created from velocity_xmatch.jl and analyzes it using MCMC to estimate the velocity dispersion and search for any possible velocity gradients.
+"""
+
+# ╔═╡ 34e43f4a-bcff-41cb-92c4-0c8d600fd053
+import CSV
+
+# ╔═╡ 9e9ba645-b780-4afa-b305-a2b1d8a97220
+import StatsBase: quantile, mean, std, median, sem
+
+# ╔═╡ d2888213-61e3-4a6f-872b-48a075640ef5
+import TOML
+
+# ╔═╡ b00b7e2b-3a72-466f-ac09-86cdda1e4a9c
+CairoMakie.activate!(type=:png)
+
+# ╔═╡ 2ab0018f-1628-4f5e-b7da-370eb20c00d0
+module RVUtils
+	include("../../rv_utils.jl")
+end
+
+# ╔═╡ 5ec475a1-14bb-40f6-856a-69fa9efe087a
+⊕ = RVUtils.:⊕
+
+# ╔═╡ 7e086680-983e-429b-a917-5330f88c7c55
+module GaiaFilters
+	include("../../../utils/gaia_filters.jl")
+end
+
+# ╔═╡ d4eb6d0f-4fe0-4e9d-b617-7a41f78da940
+md"""
+# Loading data tables
+"""
+
+# ╔═╡ 3eb74a2e-ca74-4145-a2a4-7ffbe5fffe94
+obs_properties = TOML.parsefile(ENV["DWARFS_ROOT"] * "/observations/bootes3/observed_properties.toml")
+
+# ╔═╡ c4b08cab-0039-4782-8080-b9ef3a6f6d98
+filt_params = GaiaFilters.GaiaFilterParams(obs_properties, filename="../data/j24_$j24_sample.fits")
+
+# ╔═╡ feca883d-cbb9-435a-966d-89fefb69ca49
+j24 = GaiaFilters.read_gaia_stars(filt_params)
+
+# ╔═╡ 1bc6b7f5-4884-479d-b4be-54f28c2e0a8a
+f_sat = TOML.parsefile("../j+24_fsat.toml")[j24_sample]["f_sat"]
+
+# ╔═╡ 019d1bfd-b6d0-4178-b644-cc6ca45b66ae
+mean(j24.PSAT[j24.F_BEST .=== 1.])
+
+# ╔═╡ 66c35421-f6d3-4b2d-86e4-319f5476b222
+σv = obs_properties["sigma_v"]
+
+# ╔═╡ 2d110151-f7f3-4b09-8684-a2fc4327814b
+Δv_gsr = RVUtils.rv_gsr_shift(obs_properties["ra"], obs_properties["dec"])
+
+# ╔═╡ 84509e42-8484-410a-8a76-38473b9f4b71
+rv0 = obs_properties["radial_velocity"] .- Δv_gsr
+
+# ╔═╡ 66682e56-4ad9-4823-99da-fc599882eb41
+rv_all = read_fits(joinpath(data_dir, rv_file))
+
+# ╔═╡ b20720ac-2787-4cf7-a44b-0cb5293a00b9
+@assert :L_PM_SAT ∉ names(rv_all)
+
+# ╔═╡ 097a102b-d6a6-444d-9761-ecb06d64d07f
+icrs0 = RVUtils.icrs(obs_properties)
+
+# ╔═╡ fc3ec4d1-5812-4177-90b8-29bbba72d720
+gsr0 = lguys.transform(GSR, icrs0)
+
+# ╔═╡ 4dac920b-8252-48a7-86f5-b9f96de6aaa0
+rv_meas = let
+	rv_meas = rv_all[.!ismissing.(rv_all.source_id), :]
+	disallowmissing!(rv_meas, :source_id)
+	leftjoin!(rv_meas, j24, on=:source_id, makeunique=true)
+	rv_meas = rv_meas[rv_meas.F_BEST .== 1.0, :]
+
+	RVUtils.add_gsr!(rv_meas, 
+					 distance=obs_properties["distance"],
+					pmra=obs_properties["pmra"], pmdec=obs_properties["pmdec"])
+
+	Δrv = RVUtils.rv_correction(rv_meas.ra, rv_meas.dec, gsr0)
+
+	rv_meas[!, :delta_rv] = Measurements.value.(Δrv)
+	rv_meas[!, :delta_rv_err] = Measurements.uncertainty.(Δrv)
+
+	
+	rv_meas[:, :vz] .= rv_meas.radial_velocity_gsr .+ rv_meas.delta_rv
+	rv_meas[:, :vz_err] .= rv_meas.RV_err
+
+	RVUtils.add_PSAT_RV!(rv_meas; sigma_v=σv, radial_velocity_gsr=rv0, f_sat=f_sat)
+	
+	rv_meas
+end
+
+# ╔═╡ b76a094e-11a4-4d43-869b-27c7f0c2eaee
+hist(rv_meas.delta_rv_err ./ rv_meas.RV_err)
+
+# ╔═╡ c28071fe-6077-43ad-b930-604483d5eb28
+md"""
+## Membership
+"""
+
+# ╔═╡ 733fe42e-b7a5-4285-8c73-9a41e4488d40
+memb_filt = (rv_meas.PSAT_RV .> psat_min) .&
+	rv_meas.F_scatter .&
+	(rv_meas.F_BEST .== 1)
+
+# ╔═╡ cc6c65db-ef57-4745-8ada-e11427274a77
+memb_stars = rv_meas[memb_filt, :]
+
+# ╔═╡ 2c69332e-9341-4805-9066-10df38fa32fe
+"$(outname)_memb.fits"
+
+# ╔═╡ f9f5759a-9a4a-4765-81c8-d513c8a0a181
+write_fits(joinpath(data_dir, "$(outname).fits"), memb_stars, overwrite=true)
+
+# ╔═╡ 92f4c8b3-442d-4a56-b05f-1fc547231508
+"RV_gmos" ∈ names(memb_stars) && memb_stars[.!ismissing.(memb_stars.RV_gmos), [:source_id, :ra, :dec, :PSAT]]
+
+# ╔═╡ ea3d420f-00f8-4ca2-a49d-e26b48e50afd
+nonmemb_stars = rv_meas[.!memb_filt, :]
+
+# ╔═╡ a162219f-df5a-41d8-bf54-927a355f6431
+write_fits(joinpath(data_dir, "$(outname)_nonmemb.fits"), nonmemb_stars, overwrite=true)
+
+# ╔═╡ 55ce0f69-8a96-4bbb-a59f-ee6503624ea6
+md"""
+# Numbers
+"""
+
+# ╔═╡ 31a6c2e4-538c-4adc-bbda-5043680b17f7
+extrema(memb_stars.RV)
+
+# ╔═╡ 3377f632-713d-4fec-84a9-b0211b02cb43
+median_err = median(memb_stars.RV_err)
+
+# ╔═╡ 064dff05-192d-41bb-993f-5fb5abc36ecd
+number_qual = sum(rv_meas.F_scatter .& (rv_meas.F_BEST .== 1.0))
+
+# ╔═╡ a1938588-ca40-4844-ab82-88c4254c435b
+number_memb = length(memb_stars.RV)
+
+# ╔═╡ b8ee2ea3-98aa-44c4-a309-1a356feb0686
+sample_info = OrderedDict(
+	"median_err" => median_err,
+	"number_qual" => number_qual,
+	"number_memb" => number_memb,
+)
+
+# ╔═╡ b1230b9d-e3ea-4330-b7f5-e708f08db51c
+open(joinpath(data_dir, "$outname.info.toml"), "w") do f
+	TOML.print(f, sample_info)
+end
+
+# ╔═╡ cb3bc2ab-8ed7-493d-9868-f793fe24bc42
+md"""
+# Plots
+"""
+
+# ╔═╡ 081c30c7-28e9-4155-80a5-d35317ba926a
+hist(nonmemb_stars.RV)
+
+# ╔═╡ 68edc01b-496e-466e-9980-83a586b0bb82
+hist(memb_stars.RV)
+
+# ╔═╡ fb52ac04-1483-471f-a164-9bbe15464378
+scatter(nonmemb_stars.xi, nonmemb_stars.eta)
+
+# ╔═╡ 6493a62d-cdb7-4831-9da6-25035e3cb7c5
+scatter(memb_stars.xi, memb_stars.eta, markersize=2, alpha=0.4)
+
+# ╔═╡ 732a4a2d-963b-44a8-aa81-c008906728fe
+sum(memb_stars.P_SAT_deimos .> 0.2)
+
+# ╔═╡ 1a2a5771-a6c5-4481-8c99-e82c3d50160c
+sum(nonmemb_stars.P_SAT_deimos .> 0.2)
+
+# ╔═╡ 82105731-560f-4716-8e4c-2a2539cf6f08
+rv_all[rv_all.P_SAT_deimos .> 0.5, :]
+
+# ╔═╡ 3395339f-4679-4c2a-a420-319cddc64706
+
+
+# ╔═╡ Cell order:
+# ╟─3ed8c28f-5908-42dc-a56b-24a9b2685a07
+# ╠═50488b8f-6886-4191-8778-af66929f1445
+# ╟─680e7f76-cb4d-40d6-9a9f-d4672427a633
+# ╠═3e0eb6d1-6be4-41ec-98a5-5e9167506e61
+# ╠═86fe351f-ef12-474a-85cc-c10c22a65e77
+# ╟─7330c75e-1bf9-476a-8274-ebc86d555e6f
+# ╟─6bec7416-40c8-4e2b-9d3d-14aa19e5642d
+# ╠═04bbc735-e0b4-4f0a-9a83-e50c8b923caf
+# ╠═34e43f4a-bcff-41cb-92c4-0c8d600fd053
+# ╠═93838644-cad6-4df3-b554-208b7afeb3b8
+# ╠═bd6dfd17-02ee-4855-be37-fecfdab6776f
+# ╠═72f1febc-c6ea-449a-8cec-cd0e49c4e20c
+# ╠═d7d439be-b77b-4a3d-9fb6-7dd7583dc52e
+# ╠═9e9ba645-b780-4afa-b305-a2b1d8a97220
+# ╠═035cdedb-da23-4cfd-aa19-a3aff089d3ac
+# ╠═d2888213-61e3-4a6f-872b-48a075640ef5
+# ╠═b00b7e2b-3a72-466f-ac09-86cdda1e4a9c
+# ╠═2ab0018f-1628-4f5e-b7da-370eb20c00d0
+# ╠═5ec475a1-14bb-40f6-856a-69fa9efe087a
+# ╠═7e086680-983e-429b-a917-5330f88c7c55
+# ╟─d4eb6d0f-4fe0-4e9d-b617-7a41f78da940
+# ╠═3eb74a2e-ca74-4145-a2a4-7ffbe5fffe94
+# ╠═c4b08cab-0039-4782-8080-b9ef3a6f6d98
+# ╠═feca883d-cbb9-435a-966d-89fefb69ca49
+# ╠═1bc6b7f5-4884-479d-b4be-54f28c2e0a8a
+# ╠═019d1bfd-b6d0-4178-b644-cc6ca45b66ae
+# ╠═66c35421-f6d3-4b2d-86e4-319f5476b222
+# ╠═2d110151-f7f3-4b09-8684-a2fc4327814b
+# ╠═84509e42-8484-410a-8a76-38473b9f4b71
+# ╠═66682e56-4ad9-4823-99da-fc599882eb41
+# ╠═b20720ac-2787-4cf7-a44b-0cb5293a00b9
+# ╠═fc3ec4d1-5812-4177-90b8-29bbba72d720
+# ╠═097a102b-d6a6-444d-9761-ecb06d64d07f
+# ╠═4dac920b-8252-48a7-86f5-b9f96de6aaa0
+# ╠═b76a094e-11a4-4d43-869b-27c7f0c2eaee
+# ╟─c28071fe-6077-43ad-b930-604483d5eb28
+# ╠═733fe42e-b7a5-4285-8c73-9a41e4488d40
+# ╠═cc6c65db-ef57-4745-8ada-e11427274a77
+# ╠═2c69332e-9341-4805-9066-10df38fa32fe
+# ╠═f9f5759a-9a4a-4765-81c8-d513c8a0a181
+# ╠═92f4c8b3-442d-4a56-b05f-1fc547231508
+# ╠═ea3d420f-00f8-4ca2-a49d-e26b48e50afd
+# ╠═a162219f-df5a-41d8-bf54-927a355f6431
+# ╟─55ce0f69-8a96-4bbb-a59f-ee6503624ea6
+# ╠═31a6c2e4-538c-4adc-bbda-5043680b17f7
+# ╠═3377f632-713d-4fec-84a9-b0211b02cb43
+# ╠═064dff05-192d-41bb-993f-5fb5abc36ecd
+# ╠═a1938588-ca40-4844-ab82-88c4254c435b
+# ╠═b8ee2ea3-98aa-44c4-a309-1a356feb0686
+# ╠═b1230b9d-e3ea-4330-b7f5-e708f08db51c
+# ╟─cb3bc2ab-8ed7-493d-9868-f793fe24bc42
+# ╠═081c30c7-28e9-4155-80a5-d35317ba926a
+# ╠═68edc01b-496e-466e-9980-83a586b0bb82
+# ╠═fb52ac04-1483-471f-a164-9bbe15464378
+# ╠═6493a62d-cdb7-4831-9da6-25035e3cb7c5
+# ╠═732a4a2d-963b-44a8-aa81-c008906728fe
+# ╠═1a2a5771-a6c5-4481-8c99-e82c3d50160c
+# ╠═82105731-560f-4716-8e4c-2a2539cf6f08
+# ╠═3395339f-4679-4c2a-a420-319cddc64706
